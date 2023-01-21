e4211d1e1720184d6367e62e69b998be431e2027
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-01-22 03:17:07 +0700

    Adding content and making responsive design about header, menu, and detail recipes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+// ! MAIN SECTION
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 if (window.innerWidth >= 900) {
   navMenu.classList.add("nav-menu");
 }
-window.addEventListener("resize", function() {
+window.addEventListener("resize", function () {
   if (window.innerWidth <= 900) {
     document.querySelector(".logo").innerHTML = "Fairuz";
   }
@@ -18,23 +19,54 @@ hamburger.addEventListener("click", function () {
     navMenu.classList.remove("hidden");
     navMenu.classList.add("nav-menu");
   }
-
-  console.log(navMenu);
 });
 
-var data = [
+// ! PAGE MENU SECTION
+window.onscroll = function(){
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+  if(window.pageYOffset>fixedNav){
+    header.classList.add("")
+  }
+}
+const data = [
   { title: "item 1", description: "100" },
   { title: "item 2", description: "200" },
   { title: "item 3", description: "300" },
 ];
 
-var list = document.getElementsByClassName("mapping");
-console.log(list);
+// const list = document.querySelector(".mapping");
+// window.addEventListener("DOMContentLoaded", function () {
+// const showHTML = data.map((item, index) => {
+//   return `
+//   <div class="content-menu" role="button">
+//   <a href="">
+//     <img className="menu-img" src="../src/ayam kecap.jpg" />
+//     <div class="details-menu">
+//       <h3 className="menu-title"></h3>
+//       <p className="menu-description"></p>
+//     </div>
+//   </a>
+// </div>
+//   `;
+// });
+// list.innerHTML = showHTML;}
+const myContent = document.getElementById("mapping");
 
-for (var i = 0; i < data.length; i++) {
-  var item = data[i];
-  var title = list.getElementsByClassName("menu-title")[i];
-  var description = list.getElementsByClassName("menu-description")[i];
-  title.innerHTML = item.title;
-  description.innerHTML = item.description;
-}
+// window.addEventListener("DOMContentLoaded", function () {
+//   const showInHtml = data.map((project, index) => {
+//     return `
+//     <div key=${index} class="content-menu" role="button" width="300" height="300">
+//       <a href="">
+//         <img className="menu-img" src="../src/ayam kecap.jpg" />
+//         <div class="details-menu">
+//           <h3 className="menu-title">${project.title}</h3>
+//           <p className="menu-description">${project.description}</p>
+//         </div>
+//       </a>
+//     </div>
+//         `;
+//   });
+//   myContent.innerHTML = showInHtml.join('');  
+//   console.log(myContent.innerHTML);
+// });
