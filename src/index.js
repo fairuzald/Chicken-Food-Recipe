d5d42157d195f654289c6ee6905ab54a112b2a2f
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-01-22 00:21:27 +0700

    Adding fixing responsive design on main page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+if (window.innerWidth >= 900) {
+  navMenu.classList.add("nav-menu");
+}
+window.addEventListener("resize", function() {
+  if (window.innerWidth <= 900) {
+    document.querySelector(".logo").innerHTML = "Fairuz";
+  }
+});
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburgeractive");
+
+  if (navMenu.className == "nav-menu") {
+    navMenu.classList.remove("nav-menu");
+    navMenu.classList.add("hidden");
+  } else {
+    navMenu.classList.remove("hidden");
+    navMenu.classList.add("nav-menu");
+  }
+
+  console.log(navMenu);
+});
+
 var data = [
   { title: "item 1", description: "100" },
   { title: "item 2", description: "200" },
